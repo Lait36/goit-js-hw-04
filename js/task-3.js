433424f663d890a6b5c4c4216aacebd727cfd6cd
Lait36 <118002242+Lait36@users.noreply.github.com>
2024-01-05 21:22:11 +0200

    finished task

---
+++ js/task-3.js
@@ -0,0 +1,25 @@
+let profile = {
+  username: 'Jacob',
+  playTime: 300,
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+
+  getInfo() {
+    const message = `${profile.username} has ${profile.playTime} active hours!`;
+    return message;
+  },
+};
+// 
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
